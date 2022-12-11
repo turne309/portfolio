5381fc8396ff155af35e9a677226b76a492439f5
turne309 <jaydonmturner@gmail.com>
2022-12-10 19:01:54 -0500

    Minor update

--- script.js
+++ script.js
@@ -1,31 +1,88 @@
 // Mouse Circle
-const mouseCircle = document.querySelector(".mouse-circle");
-const mouseDot = document.querySelector(".mouse-dot");
+let mouseCircle = document.querySelector(".mouse-circle");
+let mouseDot = document.querySelector(".mouse-dot");
 
 const mouseCircleFn = (x, y) => {
   mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
+// End of mouse circle
+
+// Animated circles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+const z = 20;
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
+    circles.forEach((circles) => {
+      circles.style.left = `${z}px`;
+    });
+    mainImg.style.left = `${z}px`;
+  } else if (x > mX) {
+    circles.forEach((circles) => {
+      circles.style.left = `-${z}px`;
+    });
+    mainImg.style.left = `-${z}px`;
+  }
+  if (y < mY) {
+    circles.forEach((circles) => {
+      circles.style.top = `${z}px`;
+    });
+    mainImg.style.top = `${z}px`;
+  } else if (y > mY) {
+    circles.forEach((circles) => {
+      circles.style.top = `-${z}px`;
+    });
+    mainImg.style.top = `-${z}px`;
+  }
+
+  mX = e.clientX;
+  mY = e.clientY;
+};
+// End of Anim Circles
 
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
 
   mouseCircleFn(x, y);
+  animateCircles(e, x, y);
 });
 
 document.body.addEventListener("mouseleave", () => {
   mouseCircle.style.opacity = "0";
   mouseDot.style.opacity = "0";
 });
-// End of mouse circle
+
+// Main Button
+const mainBtn = document.querySelector(".main-btn");
+let ripple;
+
+mainBtn.addEventListener("mouseenter", (e) => {
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.right = `${top}px`;
+  mainBtn.prepend(ripple);
+});
+
+mainBtn.addEventListener("mouseleave", () => {
+  mainBtn.removeChild(ripple);
+});
+// End of Main Button
 
 //
 //
 //
 //
 //
-//
+//BtwpleapleaShoul
 //
 //
 
